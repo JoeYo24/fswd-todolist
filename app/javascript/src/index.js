bab34bd8c1ad011493e4fda9d93a811a9597d078
JoeYo24 <jjsteffen@hotmail.com>
2023-07-08 15:42:36 -0400

    now it's finally done

--- app/javascript/src/index.js
+++ app/javascript/src/index.js
@@ -4,20 +4,20 @@ import { indexTasks, postTask, deleteTask, markTaskComplete, markTaskActive } fr
 $(document).ready(function () {
   // Retrieve and render tasks
   indexTasks(function (response) {
-    var htmlString = response.tasks.map(function (task) {
-      return `<div class="col-12 mb-3 p-2 border rounded task" data-id="${task.id}">
-        <div class="d-flex justify-content-between align-items-center">
-          <span>${task.content}</span>
-          <div>
-            <button class="btn btn-xs btn-danger remove">X</button>
-            <button class="btn btn-xs btn-success mark-complete">Mark Complete</button>
-            <button class="btn btn-xs btn-primary mark-active">Mark Active</button>
-          </div>
-        </div>
-      </div>`;
+    var tasks = response.tasks;
+    var activeTasks = tasks.filter(task => !task.completed);
+    var completedTasks = tasks.filter(task => task.completed);
+
+    var activeTasksHtml = activeTasks.map(function (task) {
+      return generateTaskHtml(task, false);
     });
 
-    $("#tasks").html(htmlString.join(""));
+    var completedTasksHtml = completedTasks.map(function (task) {
+      return generateTaskHtml(task, true);
+    });
+
+    var htmlString = activeTasksHtml.concat(completedTasksHtml).join("");
+    $("#tasks").html(htmlString);
   });
 
   // Add task form submission
@@ -29,21 +29,21 @@ $(document).ready(function () {
       console.log('Task posted successfully', response);
       // Refresh tasks after successful creation
       indexTasks(function (response) {
-        var htmlString = response.tasks.map(function (task) {
-          return `<div class="col-12 mb-3 p-2 border rounded task" data-id="${task.id}">
-            <div class="d-flex justify-content-between align-items-center">
-              <span>${task.content}</span>
-              <div>
-                <button class="btn btn-xs btn-danger remove">X</button>
-                <button class="btn btn-xs btn-success mark-complete">Mark Complete</button>
-                <button class="btn btn-xs btn-primary mark-active">Mark Active</button>
-              </div>
-            </div>
-          </div>`;
+        var tasks = response.tasks;
+        var activeTasks = tasks.filter(task => !task.completed);
+        var completedTasks = tasks.filter(task => task.completed);
+
+        var activeTasksHtml = activeTasks.map(function (task) {
+          return generateTaskHtml(task, false);
         });
 
-        newTaskContent = $("#new-task-content").val("");
-        $("#tasks").html(htmlString.join(""));
+        var completedTasksHtml = completedTasks.map(function (task) {
+          return generateTaskHtml(task, true);
+        });
+
+        var htmlString = activeTasksHtml.concat(completedTasksHtml).join("");
+        $("#tasks").html(htmlString);
+        $("#new-task-content").val("");
       });
     }, function (error) {
       console.log('Error posting task', error);
@@ -61,20 +61,20 @@ $(document).ready(function () {
         console.log('Task deleted successfully', response);
         // Refresh tasks after successful deletion
         indexTasks(function (response) {
-          var htmlString = response.tasks.map(function (task) {
-            return `<div class="col-12 mb-3 p-2 border rounded task" data-id="${task.id}">
-              <div class="d-flex justify-content-between align-items-center">
-                <span>${task.content}</span>
-                <div>
-                  <button class="btn btn-xs btn-danger remove">X</button>
-                  <button class="btn btn-xs btn-success mark-complete">Mark Complete</button>
-                  <button class="btn btn-xs btn-primary mark-active">Mark Active</button>
-                </div>
-              </div>
-            </div>`;
+          var tasks = response.tasks;
+          var activeTasks = tasks.filter(task => !task.completed);
+          var completedTasks = tasks.filter(task => task.completed);
+
+          var activeTasksHtml = activeTasks.map(function (task) {
+            return generateTaskHtml(task, false);
           });
 
-          $("#tasks").html(htmlString.join(""));
+          var completedTasksHtml = completedTasks.map(function (task) {
+            return generateTaskHtml(task, true);
+          });
+
+          var htmlString = activeTasksHtml.concat(completedTasksHtml).join("");
+          $("#tasks").html(htmlString);
         });
       });
     }
@@ -91,20 +91,20 @@ $(document).ready(function () {
         console.log('Task marked as complete', response);
         // Refresh tasks after marking as complete
         indexTasks(function (response) {
-          var htmlString = response.tasks.map(function (task) {
-            return `<div class="col-12 mb-3 p-2 border rounded task" data-id="${task.id}">
-              <div class="d-flex justify-content-between align-items-center">
-                <span>${task.content}</span>
-                <div>
-                  <button class="btn btn-xs btn-danger remove">X</button>
-                  <button class="btn btn-xs btn-success mark-complete">Mark Complete</button>
-                  <button class="btn btn-xs btn-primary mark-active">Mark Active</button>
-                </div>
-              </div>
-            </div>`;
+          var tasks = response.tasks;
+          var activeTasks = tasks.filter(task => !task.completed);
+          var completedTasks = tasks.filter(task => task.completed);
+
+          var activeTasksHtml = activeTasks.map(function (task) {
+            return generateTaskHtml(task, false);
           });
 
-          $("#tasks").html(htmlString.join(""));
+          var completedTasksHtml = completedTasks.map(function (task) {
+            return generateTaskHtml(task, true);
+          });
+
+          var htmlString = activeTasksHtml.concat(completedTasksHtml).join("");
+          $("#tasks").html(htmlString);
         });
       });
     }
@@ -121,24 +121,41 @@ $(document).ready(function () {
         console.log('Task marked as active', response);
         // Refresh tasks after marking as active
         indexTasks(function (response) {
-          var htmlString = response.tasks.map(function (task) {
-            return `<div class="col-12 mb-3 p-2 border rounded task" data-id="${task.id}">
-              <div class="d-flex justify-content-between align-items-center">
-                <span>${task.content}</span>
-                <div>
-                  <button class="btn btn-xs btn-danger remove">X</button>
-                  <button class="btn btn-xs btn-success mark-complete">Mark Complete</button>
-                  <button class="btn btn-xs btn-primary mark-active">Mark Active</button>
-                </div>
-              </div>
-            </div>`;
+          var tasks = response.tasks;
+          var activeTasks = tasks.filter(task => !task.completed);
+          var completedTasks = tasks.filter(task => task.completed);
+
+          var activeTasksHtml = activeTasks.map(function (task) {
+            return generateTaskHtml(task, false);
           });
 
-          $("#tasks").html(htmlString.join(""));
+          var completedTasksHtml = completedTasks.map(function (task) {
+            return generateTaskHtml(task, true);
+          });
+
+          var htmlString = activeTasksHtml.concat(completedTasksHtml).join("");
+          $("#tasks").html(htmlString);
         });
       });
     }
   });
+
+  // Helper function to generate task HTML based on completion status
+  function generateTaskHtml(task, completed) {
+    var taskClass = completed ? 'completed' : 'active';
+    var taskContent = completed ? `<del>${task.content}</del>` : task.content;
+
+    return `<div class="col-12 mb-3 p-2 border rounded task ${taskClass}" data-id="${task.id}">
+      <div class="d-flex justify-content-between align-items-center">
+        <span>${taskContent}</span>
+        <div>
+          <button class="btn btn-xs btn-danger remove">X</button>
+          <button class="btn btn-xs btn-success mark-complete">Mark Complete</button>
+          <button class="btn btn-xs btn-primary mark-active">Mark Active</button>
+        </div>
+      </div>
+    </div>`;
+  }
 });
 
 
